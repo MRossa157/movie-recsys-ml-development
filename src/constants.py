22681e89deef70226b50f194af4294dcc590f333
Nikita Babaev <maximrossa1@gmail.com>
2025-01-25 18:04:20 +0400

    Massive feature engineering

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,7 @@
-DATASET_NAME = 'ml-latest-small'
-WEIGHTS_PATH = r'src/models'
+from enum import Enum
 
 
-RATINGS_PATH_SANDBOX = r'../MovieLens_20M_Dataset/rating.csv'
-MOVIE_PATH_SANDBOX = r'../MovieLens_20M_Dataset/movie.csv'
-
-
-NCF_BATCH_SIZE = 512
-NCF_MAX_EPOCHS = 10
+class ItemsFeatureTopKConfig(int, Enum):
+    """Конфигурация для ограничения количества топовых значений фич."""
+    DIRECTORS_TOP_K = 30
+    STUDIOS_TOP_K = 15
