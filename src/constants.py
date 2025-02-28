fc4ed3911daa486647f9872d3e77e2989d10a706
Nikita Babaev <maximrossa1@gmail.com>
2025-02-28 19:14:51 +0400

    Moved all non-.ipynb files to backend

--- src/constants.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ItemsFeatureTopKConfig(int, Enum):
-    """Конфигурация для ограничения количества топовых значений фич."""
-    DIRECTORS_TOP_K = 30
-    STUDIOS_TOP_K = 15
